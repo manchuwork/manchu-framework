1021c3b55d41a26397644868f931d86cfcffd691
heshen <wanggang@youzan.com>
2019-03-01 00:16:53 +0800

    修改 即词划译

--- resources/views/frontend/customize/layouts/_footer.blade.php
+++ resources/views/frontend/customize/layouts/_footer.blade.php
@@ -1,3 +1,14 @@
+<div id="Jihuajiyi">
+    <div class="iciba_switch_bar">
+        <div class="iciba_switch_bar_wrap on" id="iciba_switch_bar_wrap"><span
+                    class="iciba_switch_on">ON</span><span class="iciba_switch_slider"></span><span
+                    class="iciba_switch_off">OFF</span></div>
+    </div>
+    <i class="iciba_switch-icon"></i><span class="iciba_switch_txt"> 即划即译</span>
+
+</div>
+<link href="{{asset('vendor/laracms/css/mini.css')}}" rel="stylesheet" type="text/css">
+<script src="{{asset('vendor/laracms/js/dragword.js')}}"></script>
 <div class="fly-footer">
     <p>Copyright {{config("system.common.basic.create_year")}} - {{date('Y')}} {{ config("system.common.basic.copyright") }} All Rights Reserved</p>
     <p><a href="{{ config("system.common.basic.icp_link") }}" target="_blank">{{ config("system.common.basic.icp") }}</a></p>
